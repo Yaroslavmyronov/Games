7d1e5787961a3261a54fd6bd8e3204b0b7e058fa
YaroslavMyronov <ytube.noobtv@gmail.com>
2022-09-09 10:49:51 +0300

    Who shot faster

---
+++ Who shot faster.py
@@ -0,0 +1,25 @@
+
+# def whos_first1(p1, p2):
+#     no_space1 = p1.rstrip()
+#     simvol1 = len(no_space1) - 6
+#     no_space2 = p2.rstrip()
+#     simvol2 = len(no_space2) - 6
+#     if simvol1 == simvol2:
+#         print('Ничья')
+#     elif simvol1 < simvol2:
+#         print('Победил p1')
+#     elif simvol1 > simvol2:
+#         print('Победил p2')
+# whos_first1("     Bang!   ", "     Bang!   ")
+
+
+def whos_first2(p1, p2):
+    if p1.find('B') > p2.find('B'): # с помощь метода find находим вхождение В у первого игрока и сравниваем с вхождением у другого игрока
+        print('Cтреляет быстрее p2')
+    elif p1.find('B') < p2.find('B'): 
+        print('Cтреляет быстрее p2')
+    elif p1.find('B') == p2.find('B'): 
+        print('Ничья')
+
+
+whos_first2("               Bang! ", "             Bang!   ")
--- guess the number.py
+++ guess the number.py
@@ -1,17 +1,17 @@
 import random
 
 tries = 0
-comp_random = random.randint(1,50)
+comp_random = random.randint(1,50) #Вернёт целое N , подходящее под описание: min <= N <= max from random
 while tries <=6:
     user_input = int(input('Введите число от 1 до 50 '))
     tries+=1
-    if user_input == comp_random:
+    if user_input == comp_random: #проверка на число которое ввел пользовтель и число которое сгенерировал компьютер
         print('Ты угадал')
         break
-    elif user_input > comp_random:
+    elif user_input > comp_random: 
         print('Попробуй ввести число по меньше) ')
     elif user_input < comp_random:
         print('Попробуй ввести число по больше )')
-    elif user_input != comp_random and tries == 6:
+    elif user_input != comp_random and tries == 6: # условие когда не совпадает ввод пользователя и компьютера и попытки исчерпаны
         print('Твои попытки закончились, попробуй еще раз')
         break
